5abd25bb66f9c691505d0c2418febee1712cf0d8
Cumbres <Comitecumbresdequilpue@gmail.com>
2025-06-11 23:03:34 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,21 @@
-// firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Importa las funciones necesarias del SDK de Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.x.x/firebase-firestore.js";
-// No importamos getAnalytics aquí porque lo manejarás en el script principal si es necesario.
-
-// Tu configuración de la aplicación web de Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDexbXcg72xywNAaJ_RsvacG96lNxiBRyY",
   authDomain: "gestordetareas-383f2.firebaseapp.com",
   projectId: "gestordetareas-383f2",
   storageBucket: "gestordetareas-383f2.firebasestorage.app",
   messagingSenderId: "792456592792",
-  appId: "1:792456592792:web:8dd8b44c17579a3d393fed",
-  measurementId: "G-F16XM9PHFM"
+  appId: "1:792456592792:web:6dd820fbda79534a393fed",
+  measurementId: "G-2PQLVREQVZ"
 };
 
-// Inicializa Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Obtiene la instancia de Firestore
-const db = getFirestore(app);
-
-// Exporta la instancia de Firestore para que pueda ser utilizada en otros archivos JS
-export { db };
+const analytics = getAnalytics(app);
